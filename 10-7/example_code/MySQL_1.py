3f2b6d7ff8919e872952416e76970e47f51424c7
范 范 <applepie9687@gmail.com>
2024-10-06 18:17:35 +0800

    新增10-7檔案

---
+++ 10-7/example_code/MySQL_1.py
@@ -0,0 +1,2 @@
+# see instalation guide https://www.itread01.com/articles/1476681060.html 
+# download mySQL community server at https://dev.mysql.com/downloads/mysql/
---
+++ 10-7/example_code/MySQL_query.py
@@ -0,0 +1,17 @@
+# install MySQL 
+# see instalation guide https://www.itread01.com/articles/1476681060.html 
+# download mySQL community server at https://dev.mysql.com/downloads/mysql/
+# Install MySQL workbench
+# install "mysqlclient" by running "pip install --only -binary :all: mysqlclient" 
+# see https://stackoverflow.com/questions/29846087/microsoft-visual-c-14-0-is-required-unable-to-find-vcvarsall-bat
+
+import MySQLdb
+db=MySQLdb.connect(host="localhost", user="newuser", password="Test1234", db="test_db")
+print ("connected")
+cur=db.cursor();
+#cur.execute("Select * from employee")
+
+# 該資料庫 (schema)的權限必須開放select (custom)給該user
+cur.execute("Select * from employee")
+for emp in cur.fetchall():
+    print(emp)
